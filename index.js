01844955ba65ab40a175182714521b001eb7fb76
Christopher Saunders <topher6345@gmail.com>
2020-04-10 21:33:34 -0700

    1586579614

--- index.js
+++ index.js
@@ -259,7 +259,7 @@ var GameOfLife = /** @class */ (function () {
 var sel = function (s) {
     return document.querySelector(s);
 };
-var canvas = sel("#2dcanvas");
+var canvas = sel("#twodcanvas");
 var gameOfLife = new GameOfLife(750, canvas);
 var msPast = null;
 var msPerFrame = 41.666666666666664;
@@ -415,3 +415,279 @@ sel("#noiseOff").addEventListener("change", function (e) {
 sel("#gameType").addEventListener("change", function (e) {
     gameOfLife.mode = e.target.value;
 });
+//from http://webglfundamentals.org/webgl/lessons/webgl-boilerplate.html
+/**
+ * Creates and compiles a shader.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} shaderSource The GLSL source code for the shader.
+ * @param {number} shaderType The type of shader, VERTEX_SHADER or
+ *     FRAGMENT_SHADER.
+ * @return {!WebGLShader} The shader.
+ */
+function compileShader(gl, shaderSource, shaderType) {
+    // Create the shader object
+    var shader = gl.createShader(shaderType);
+    // Set the shader source code.
+    gl.shaderSource(shader, shaderSource);
+    // Compile the shader
+    gl.compileShader(shader);
+    // Check if it compiled
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (!success) {
+        // Something went wrong during compilation; get the error
+        throw "could not compile shader:" + gl.getShaderInfoLog(shader);
+    }
+    return shader;
+}
+/**
+ * Creates a program from 2 shaders.
+ *
+ * @param {!WebGLRenderingContext) gl The WebGL context.
+ * @param {!WebGLShader} vertexShader A vertex shader.
+ * @param {!WebGLShader} fragmentShader A fragment shader.
+ * @return {!WebGLProgram} A program.
+ */
+function createProgram(gl, vertexShader, fragmentShader) {
+    // create a program.
+    var program = gl.createProgram();
+    // attach the shaders.
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    // link the program.
+    gl.linkProgram(program);
+    // Check if it linked.
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (!success) {
+        // something went wrong with the link
+        throw "program filed to link:" + gl.getProgramInfoLog(program);
+    }
+    return program;
+}
+/**
+ * Creates a shader from the content of a script tag.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} scriptId The id of the script tag.
+ * @param {string} opt_shaderType. The type of shader to create.
+ *     If not passed in will use the type attribute from the
+ *     script tag.
+ * @return {!WebGLShader} A shader.
+ */
+function createShaderFromScript(gl, scriptId, opt_shaderType) {
+    // look up the script tag by id.
+    var shaderScript = document.getElementById(scriptId);
+    if (!shaderScript) {
+        throw "*** Error: unknown script element" + scriptId;
+    }
+    // extract the contents of the script tag.
+    var shaderSource = shaderScript.text;
+    // If we didn't pass in a type, use the 'type' from
+    // the script tag.
+    if (!opt_shaderType) {
+        if (shaderScript.type == "x-shader/x-vertex") {
+            opt_shaderType = gl.VERTEX_SHADER.toString();
+        }
+        else if (shaderScript.type == "x-shader/x-fragment") {
+            opt_shaderType = gl.FRAGMENT_SHADER.toString();
+        }
+        else if (!opt_shaderType) {
+            throw "*** Error: shader type not set";
+        }
+    }
+    return compileShader(gl, shaderSource, parseInt(opt_shaderType));
+}
+/**
+ * Creates a program from 2 script tags.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} vertexShaderId The id of the vertex shader script tag.
+ * @param {string} fragmentShaderId The id of the fragment shader script tag.
+ * @return {!WebGLProgram} A program
+ */
+function createProgramFromScripts(gl, vertexShaderId, fragmentShaderId) {
+    var vertexShader = createShaderFromScript(gl, vertexShaderId);
+    var fragmentShader = createShaderFromScript(gl, fragmentShaderId);
+    return createProgram(gl, vertexShader, fragmentShader);
+}
+/**
+ * Created by ghassaei on 2/20/16.
+ */
+var gl;
+var ccanvas;
+var lastState;
+var currentState;
+var frameBuffer;
+var resizedLastState;
+var resizedCurrentState;
+var width;
+var height;
+var flipYLocation;
+var textureSizeLocation;
+var mouseCoordLocation;
+var paused = false; //while window is resizing
+window.onload = initGL;
+function initGL() {
+    // Get A WebGL context
+    ccanvas = document.getElementById("glcanvas");
+    ccanvas.width = ccanvas.clientWidth;
+    ccanvas.height = ccanvas.clientHeight;
+    ccanvas.onmousemove = onMouseMove;
+    ccanvas.ontouchmove = onTouchMove;
+    window.onresize = onResize;
+    gl =
+        ccanvas.getContext("webgl", {
+            antialias: false
+        }) ||
+            ccanvas.getContext("experimental-webgl", {
+                antialias: false
+            });
+    if (!gl) {
+        alert("Could not initialize WebGL, try another browser");
+        return;
+    }
+    //setpixelated(ccanvas.getContext('2d'));
+    //function setpixelated(context){
+    //    context['imageSmoothingEnabled'] = false;       /* standard */
+    //    context['mozImageSmoothingEnabled'] = false;    /* Firefox */
+    //    context['oImageSmoothingEnabled'] = false;      /* Opera */
+    //    context['webkitImageSmoothingEnabled'] = false; /* Safari */
+    //    context['msImageSmoothingEnabled'] = false;     /* IE */
+    //}
+    gl.disable(gl.DEPTH_TEST);
+    // setup a GLSL program
+    var program = createProgramFromScripts(gl, "2d-vertex-shader", "2d-fragment-shader");
+    gl.useProgram(program);
+    // look up where the vertex data needs to go.
+    var positionLocation = gl.getAttribLocation(program, "a_position");
+    // Create a buffer for positions
+    var bufferPos = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferPos);
+    gl.enableVertexAttribArray(positionLocation);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        -1.0,
+        -1.0,
+        1.0,
+        -1.0,
+        -1.0,
+        1.0,
+        -1.0,
+        1.0,
+        1.0,
+        -1.0,
+        1.0,
+        1.0,
+    ]), gl.STATIC_DRAW);
+    //flip y
+    flipYLocation = gl.getUniformLocation(program, "u_flipY");
+    //set texture location
+    var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
+    textureSizeLocation = gl.getUniformLocation(program, "u_textureSize");
+    mouseCoordLocation = gl.getUniformLocation(program, "u_mouseCoord");
+    // provide texture coordinates for the rectangle.
+    var texCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        0.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+        0.0,
+        1.0,
+        1.0,
+    ]), gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(texCoordLocation);
+    gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+    onResize();
+    lastState = resizedLastState;
+    currentState = resizedCurrentState;
+    resizedLastState = null;
+    resizedCurrentState = null;
+    frameBuffer = gl.createFramebuffer();
+    gl.bindTexture(gl.TEXTURE_2D, lastState); //original texture
+    render();
+}
+function makeRandomArray(rgba) {
+    var numPixels = rgba.length / 4;
+    var probability = 0.15;
+    for (var i = 0; i < numPixels; i++) {
+        var ii = i * 4;
+        var state = Math.random() < probability ? 1 : 0;
+        rgba[ii] = rgba[ii + 1] = rgba[ii + 2] = state ? 255 : 0;
+        rgba[ii + 3] = 255;
+    }
+    return rgba;
+}
+function makeTexture(gl) {
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // Set the parameters so we can render any size image.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    return texture;
+}
+function render() {
+    if (!paused) {
+        if (resizedLastState) {
+            lastState = resizedLastState;
+            resizedLastState = null;
+        }
+        if (resizedCurrentState) {
+            currentState = resizedCurrentState;
+            resizedCurrentState = null;
+        }
+        // don't y flip images while drawing to the textures
+        gl.uniform1f(flipYLocation, 1);
+        step();
+        gl.uniform1f(flipYLocation, -1); // need to y flip for ccanvas
+        gl.bindTexture(gl.TEXTURE_2D, lastState);
+        //draw to ccanvas
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.bindTexture(gl.TEXTURE_2D, lastState);
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+    window.requestAnimationFrame(render);
+}
+function step() {
+    gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, currentState, 0);
+    gl.bindTexture(gl.TEXTURE_2D, lastState);
+    gl.drawArrays(gl.TRIANGLES, 0, 6); //draw to framebuffer
+    var temp = lastState;
+    lastState = currentState;
+    currentState = temp;
+}
+function onResize() {
+    paused = true;
+    ccanvas.width = ccanvas.clientWidth;
+    ccanvas.height = ccanvas.clientHeight;
+    width = ccanvas.clientWidth;
+    height = ccanvas.clientHeight;
+    gl.viewport(0, 0, width, height);
+    // set the size of the texture
+    gl.uniform2f(textureSizeLocation, width, height);
+    //texture for saving output from frag shader
+    resizedCurrentState = makeTexture(gl);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    resizedLastState = makeTexture(gl);
+    //fill with random pixels
+    var rgba = new Uint8Array(width * height * 4);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, makeRandomArray(rgba));
+    paused = false;
+}
+function onMouseMove(e) {
+    gl.uniform2f(mouseCoordLocation, e.clientX / width, e.clientY / height);
+}
+function onTouchMove(e) {
+    e.preventDefault();
+    var touch = e.touches[0];
+    gl.uniform2f(mouseCoordLocation, touch.pageX / width, touch.pageY / height);
+}
--- index.ts
+++ index.ts
@@ -355,7 +355,7 @@ class GameOfLife {
 const sel = (s: string): HTMLElement => {
   return document.querySelector(s);
 };
-const canvas = sel("#2dcanvas") as HTMLCanvasElement;
+const canvas = sel("#twodcanvas") as HTMLCanvasElement;
 const gameOfLife = new GameOfLife(750, canvas);
 
 let msPast: number = null;
@@ -571,3 +571,396 @@ sel("#noiseOff").addEventListener("change", (e) => {
 sel("#gameType").addEventListener("change", (e) => {
   gameOfLife.mode = e.target.value as any;
 });
+
+//from http://webglfundamentals.org/webgl/lessons/webgl-boilerplate.html
+
+/**
+ * Creates and compiles a shader.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} shaderSource The GLSL source code for the shader.
+ * @param {number} shaderType The type of shader, VERTEX_SHADER or
+ *     FRAGMENT_SHADER.
+ * @return {!WebGLShader} The shader.
+ */
+function compileShader(
+  gl: WebGLRenderingContext,
+  shaderSource: string,
+  shaderType: number
+) {
+  // Create the shader object
+  var shader = gl.createShader(shaderType);
+
+  // Set the shader source code.
+  gl.shaderSource(shader, shaderSource);
+
+  // Compile the shader
+  gl.compileShader(shader);
+
+  // Check if it compiled
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!success) {
+    // Something went wrong during compilation; get the error
+    throw "could not compile shader:" + gl.getShaderInfoLog(shader);
+  }
+
+  return shader;
+}
+
+/**
+ * Creates a program from 2 shaders.
+ *
+ * @param {!WebGLRenderingContext) gl The WebGL context.
+ * @param {!WebGLShader} vertexShader A vertex shader.
+ * @param {!WebGLShader} fragmentShader A fragment shader.
+ * @return {!WebGLProgram} A program.
+ */
+function createProgram(
+  gl: WebGLRenderingContext,
+  vertexShader: WebGLShader,
+  fragmentShader: WebGLShader
+) {
+  // create a program.
+  var program = gl.createProgram();
+
+  // attach the shaders.
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+
+  // link the program.
+  gl.linkProgram(program);
+
+  // Check if it linked.
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!success) {
+    // something went wrong with the link
+    throw "program filed to link:" + gl.getProgramInfoLog(program);
+  }
+
+  return program;
+}
+
+/**
+ * Creates a shader from the content of a script tag.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} scriptId The id of the script tag.
+ * @param {string} opt_shaderType. The type of shader to create.
+ *     If not passed in will use the type attribute from the
+ *     script tag.
+ * @return {!WebGLShader} A shader.
+ */
+function createShaderFromScript(
+  gl: WebGLRenderingContext,
+  scriptId: string,
+  opt_shaderType?: string
+) {
+  // look up the script tag by id.
+  var shaderScript = document.getElementById(scriptId) as any;
+  if (!shaderScript) {
+    throw "*** Error: unknown script element" + scriptId;
+  }
+
+  // extract the contents of the script tag.
+  var shaderSource = shaderScript.text;
+
+  // If we didn't pass in a type, use the 'type' from
+  // the script tag.
+  if (!opt_shaderType) {
+    if (shaderScript.type == "x-shader/x-vertex") {
+      opt_shaderType = gl.VERTEX_SHADER.toString();
+    } else if (shaderScript.type == "x-shader/x-fragment") {
+      opt_shaderType = gl.FRAGMENT_SHADER.toString();
+    } else if (!opt_shaderType) {
+      throw "*** Error: shader type not set";
+    }
+  }
+
+  return compileShader(gl, shaderSource, parseInt(opt_shaderType));
+}
+
+/**
+ * Creates a program from 2 script tags.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} vertexShaderId The id of the vertex shader script tag.
+ * @param {string} fragmentShaderId The id of the fragment shader script tag.
+ * @return {!WebGLProgram} A program
+ */
+function createProgramFromScripts(
+  gl: WebGLRenderingContext,
+  vertexShaderId: string,
+  fragmentShaderId: string
+) {
+  var vertexShader = createShaderFromScript(gl, vertexShaderId);
+  var fragmentShader = createShaderFromScript(gl, fragmentShaderId);
+  return createProgram(gl, vertexShader, fragmentShader);
+}
+
+/**
+ * Created by ghassaei on 2/20/16.
+ */
+
+let gl: WebGLRenderingContext;
+var ccanvas: HTMLCanvasElement;
+var lastState: WebGLTexture;
+var currentState: WebGLTexture;
+var frameBuffer: WebGLFramebuffer;
+
+let resizedLastState: WebGLTexture;
+var resizedCurrentState: WebGLTexture;
+
+var width: number;
+var height: number;
+
+var flipYLocation: WebGLUniformLocation;
+var textureSizeLocation: WebGLUniformLocation;
+var mouseCoordLocation: WebGLUniformLocation;
+
+var paused = false; //while window is resizing
+
+window.onload = initGL;
+
+function initGL() {
+  // Get A WebGL context
+  ccanvas = document.getElementById("glcanvas") as HTMLCanvasElement;
+  ccanvas.width = ccanvas.clientWidth;
+  ccanvas.height = ccanvas.clientHeight;
+
+  ccanvas.onmousemove = onMouseMove;
+  ccanvas.ontouchmove = onTouchMove;
+
+  window.onresize = onResize;
+  gl =
+    (ccanvas.getContext("webgl", {
+      antialias: false,
+    }) as WebGLRenderingContext) ||
+    (ccanvas.getContext("experimental-webgl", {
+      antialias: false,
+    }) as WebGLRenderingContext);
+  if (!gl) {
+    alert("Could not initialize WebGL, try another browser");
+    return;
+  }
+
+  //setpixelated(ccanvas.getContext('2d'));
+  //function setpixelated(context){
+  //    context['imageSmoothingEnabled'] = false;       /* standard */
+  //    context['mozImageSmoothingEnabled'] = false;    /* Firefox */
+  //    context['oImageSmoothingEnabled'] = false;      /* Opera */
+  //    context['webkitImageSmoothingEnabled'] = false; /* Safari */
+  //    context['msImageSmoothingEnabled'] = false;     /* IE */
+  //}
+
+  gl.disable(gl.DEPTH_TEST);
+
+  // setup a GLSL program
+  var program = createProgramFromScripts(
+    gl,
+    "2d-vertex-shader",
+    "2d-fragment-shader"
+  );
+  gl.useProgram(program);
+
+  // look up where the vertex data needs to go.
+  var positionLocation = gl.getAttribLocation(program, "a_position");
+
+  // Create a buffer for positions
+  var bufferPos = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, bufferPos);
+  gl.enableVertexAttribArray(positionLocation);
+  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      -1.0,
+      -1.0,
+      1.0,
+      -1.0,
+      -1.0,
+      1.0,
+      -1.0,
+      1.0,
+      1.0,
+      -1.0,
+      1.0,
+      1.0,
+    ]),
+    gl.STATIC_DRAW
+  );
+
+  //flip y
+  flipYLocation = gl.getUniformLocation(program, "u_flipY");
+
+  //set texture location
+  const texCoordLocation: number = gl.getAttribLocation(program, "a_texCoord");
+
+  textureSizeLocation = gl.getUniformLocation(program, "u_textureSize");
+
+  mouseCoordLocation = gl.getUniformLocation(program, "u_mouseCoord");
+
+  // provide texture coordinates for the rectangle.
+  var texCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      0.0,
+      0.0,
+      1.0,
+      0.0,
+      0.0,
+      1.0,
+      0.0,
+      1.0,
+      1.0,
+      0.0,
+      1.0,
+      1.0,
+    ]),
+    gl.STATIC_DRAW
+  );
+  gl.enableVertexAttribArray(texCoordLocation);
+  gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+  onResize();
+
+  lastState = resizedLastState;
+  currentState = resizedCurrentState;
+  resizedLastState = null;
+  resizedCurrentState = null;
+
+  frameBuffer = gl.createFramebuffer();
+
+  gl.bindTexture(gl.TEXTURE_2D, lastState); //original texture
+
+  render();
+}
+
+function makeRandomArray(rgba: Uint8Array) {
+  var numPixels = rgba.length / 4;
+  var probability = 0.15;
+  for (var i = 0; i < numPixels; i++) {
+    var ii = i * 4;
+    var state = Math.random() < probability ? 1 : 0;
+    rgba[ii] = rgba[ii + 1] = rgba[ii + 2] = state ? 255 : 0;
+    rgba[ii + 3] = 255;
+  }
+  return rgba;
+}
+
+function makeTexture(gl: WebGLRenderingContext): WebGLTexture {
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Set the parameters so we can render any size image.
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  return texture;
+}
+
+function render() {
+  if (!paused) {
+    if (resizedLastState) {
+      lastState = resizedLastState;
+      resizedLastState = null;
+    }
+    if (resizedCurrentState) {
+      currentState = resizedCurrentState;
+      resizedCurrentState = null;
+    }
+
+    // don't y flip images while drawing to the textures
+    gl.uniform1f(flipYLocation, 1);
+
+    step();
+
+    gl.uniform1f(flipYLocation, -1); // need to y flip for ccanvas
+    gl.bindTexture(gl.TEXTURE_2D, lastState);
+
+    //draw to ccanvas
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.bindTexture(gl.TEXTURE_2D, lastState);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+  }
+
+  window.requestAnimationFrame(render);
+}
+
+function step() {
+  gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
+  gl.framebufferTexture2D(
+    gl.FRAMEBUFFER,
+    gl.COLOR_ATTACHMENT0,
+    gl.TEXTURE_2D,
+    currentState,
+    0
+  );
+
+  gl.bindTexture(gl.TEXTURE_2D, lastState);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6); //draw to framebuffer
+
+  var temp = lastState;
+  lastState = currentState;
+  currentState = temp;
+}
+
+function onResize() {
+  paused = true;
+
+  ccanvas.width = ccanvas.clientWidth;
+  ccanvas.height = ccanvas.clientHeight;
+  width = ccanvas.clientWidth;
+  height = ccanvas.clientHeight;
+
+  gl.viewport(0, 0, width, height);
+
+  // set the size of the texture
+  gl.uniform2f(textureSizeLocation, width, height);
+
+  //texture for saving output from frag shader
+  resizedCurrentState = makeTexture(gl);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    width,
+    height,
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    null
+  );
+
+  resizedLastState = makeTexture(gl);
+  //fill with random pixels
+  var rgba = new Uint8Array(width * height * 4);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    width,
+    height,
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    makeRandomArray(rgba)
+  );
+
+  paused = false;
+}
+
+function onMouseMove(e: MouseEvent) {
+  gl.uniform2f(mouseCoordLocation, e.clientX / width, e.clientY / height);
+}
+
+function onTouchMove(e: TouchEvent) {
+  e.preventDefault();
+  var touch = e.touches[0];
+  gl.uniform2f(mouseCoordLocation, touch.pageX / width, touch.pageY / height);
+}

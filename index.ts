b9a2d46d7b918e8aab9166bb699fcf908f8c5396
Christopher Saunders <topher6345@gmail.com>
2021-01-29 22:11:43 -0800

    1611987103

--- index.ts
+++ index.ts
@@ -771,12 +771,15 @@ sel("#colorMode").addEventListener("change", (e) => {
       log(
         "Color mode is now Random Frame - all pixels of each frame will be the same random color"
       );
+      break;
     case "row":
       log(
         "Color mode is now Random Row - all pixels of each row will be the same random color"
       );
+      break;
     case "each":
       log("Color mode is now Random Pixel- every pixel is a new random color");
+      break;
     default:
       sel("#colorRadix").style.display = "block";
       sel('label[for="colorRadix"]').style.display = "block";

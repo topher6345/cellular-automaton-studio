90b0cb91897df37ce7528d8f0289449f0b94639a
Christopher Saunders <topher6345@gmail.com>
2021-02-07 14:48:07 -0800

    1612738087

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const { createCanvas, loadImage } = require("canvas");
-const fs = require("fs");
-const canvas = createCanvas(200, 200);
-const ctx = canvas.getContext("2d");
-
+"use strict";
+exports.__esModule = true;
+var canvas_1 = require("canvas");
+var fs = require("fs");
+var canvas = canvas_1.createCanvas(200, 200);
+var ctx = canvas.getContext("2d");
 // Write "Awesome!"
 ctx.font = "30px Impact";
 ctx.rotate(0.1);
 ctx.fillText("Awesome!", 50, 100);
-
 // Draw line under text
 var text = ctx.measureText("Awesome!");
 ctx.strokeStyle = "rgba(0,0,0,0.5)";
@@ -15,18 +15,15 @@ ctx.beginPath();
 ctx.lineTo(50, 102);
 ctx.lineTo(50 + text.width, 102);
 ctx.stroke();
-
 // Draw cat with lime helmet
-loadImage("lime-cat.jpg").then((image) => {
-  ctx.drawImage(image, 50, 0, 70, 70);
-
-  console.log('<img src="' + canvas.toDataURL() + '" />');
-
-  var regex = /^data:.+\/(.+);base64,(.*)$/;
-  var string = canvas.toDataURL();
-  var matches = string.match(regex);
-  var ext = matches[1];
-  var data = matches[2];
-  var buffer = Buffer.from(data, "base64");
-  fs.writeFileSync("data." + ext, buffer);
+canvas_1.loadImage("lime-cat.jpg").then(function (image) {
+    ctx.drawImage(image, 50, 0, 70, 70);
+    console.log('<img src="' + canvas.toDataURL() + '" />');
+    var regex = /^data:.+\/(.+);base64,(.*)$/;
+    var string = canvas.toDataURL();
+    var matches = string.match(regex);
+    var ext = matches[1];
+    var data = matches[2];
+    var buffer = Buffer.from(data, "base64");
+    fs.writeFileSync("data." + ext, buffer);
 });
